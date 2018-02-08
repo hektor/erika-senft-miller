93d741702aa1636345e54ff105183aa27ea440f0
Alder Studio <chris@alder.studio>
2018-02-08 00:50:08 -0500

    Housekeeping

--- assets/accordion.js
+++ assets/accordion.js
@@ -13,3 +13,17 @@ for (i = 0; i < acc.length; i++) {
     });
 }
 
+var acc = document.getElementsByClassName("accordion-custom");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("open");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
