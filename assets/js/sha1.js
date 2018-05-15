0d4f2e04f2bf7b7209b2af33cd8c16d4e98cb64f
Christopher Norris <chris@alder.studio>
2018-05-15 15:20:46 -0400

    Prepare for Launch
    Resolved excess JS issues, added "ongoing" projects, improved script handling.

--- assets/js/sha1.js
+++ assets/js/sha1.js
@@ -1,3 +1,36 @@
+// login
+
+"use strict";
+	function loadPage(pwd) {
+		var hash= pwd;
+		hash= Sha1.hash(pwd);
+		var url= hash + '/' ;
+        $.ajax({
+			url : url,
+			dataType : "html",
+			success : function(data) {
+				window.location= url;
+			},
+			error : function(xhr, ajaxOptions, thrownError) {
+				parent.location.hash= hash;
+				//$("#wrongPassword").show();
+				$("#password").attr("placeholder","That password is incorrect.");
+				$("#password").val("");
+			}
+		});
+	}
+ 
+	$("#loginbutton").on("click", function() {
+		loadPage($("#password").val());
+	});
+	$("#password").keypress(function(e) {
+		if (e.which == 13) {
+			
+			loadPage($("#password").val());
+		}
+	});
+$("#password").focus();
+
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 /* SHA-1 (FIPS 180-4) implementation in JavaScript                    (c) Chris Veness 2002-2017  */
 /*                                                                                   MIT Licence  */
